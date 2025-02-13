ab518b826335d1cc5ce2587c70bf19dd77a53ba9
Lacquer991 <valiullin.timur18.06.2005@gmail.com>
2025-02-14 01:38:04 +0300

    Edit ErrorWarn.sh

--- ErrorWarn.sh
+++ ErrorWarn.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-	exit 1;
+if [[ $# -ne 1 ]] 
+then
+	exit 1
 fi
 log_file="$1"
 
-if [[ ! -f "$log_file" ]]; then
-	exit 2;
+if [[ ! -f "$log_file" ]] 
+then
+	exit 2
 fi
 
 grep "WARN" "$log_file" > 1.log
